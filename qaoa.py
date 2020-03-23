8275a9b850cb796d8e558b5f7d2448f5dd651015
Franz Fuchs <franzgeorgfuchs@gmail.com>
2020-03-23 08:56:01 +0100

    update

--- qaoa.py
+++ qaoa.py
@@ -1,4 +1,5 @@
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+import numpy as np
 
 def createCircuit_MaxCut(x,G,depth,version=1,usebarrier=False):
     num_V = G.number_of_nodes()
@@ -53,3 +54,45 @@ def listSortedCosts_MaxCut(G):
     sortedcosts={k: v for k, v in sorted(costs.items(), key=lambda item: item[1])}
     return sortedcosts
 
+def costshist_MaxCut(G):
+    num_V = G.number_of_nodes()
+    costs=np.ones(2**num_V)
+    for i in range(2**num_V):
+        binstring="{0:b}".format(i).zfill(num_V)
+        y=[int(i) for i in binstring]
+        costs[i]=cost_MaxCut(y,G)
+    return costs
+
+def bins_comp_basis(data, G):
+    num_V = G.number_of_nodes()
+    bins_states = np.zeros(2**num_V)
+    num_shots=0
+    num_solutions=0
+    max_cost=0
+    average_cost=0
+    for item, binary_rep in enumerate(data):
+        integer_rep=int(str(binary_rep), 2)
+        counts=data[str(binary_rep)]
+        bins_states[integer_rep] += counts
+        num_shots+=counts
+        num_solutions+=1
+        y=[int(i) for i in str(binary_rep)]
+        lc = cost_MaxCut(y,G)
+        max_cost=max(max_cost,lc)
+        average_cost+=lc*counts
+    return bins_states, max_cost, average_cost/num_shots
+
+def expectationValue_MaxCut(data,G):
+    E=[]
+    V = list(G.nodes)
+    num_qubits = len(V)
+    for item in range(0,len(data.results)):
+        shots = data.results[item].shots
+        counts = data.results[item].data.counts
+        E.append(0)
+        for key in list(counts.__dict__.keys()):
+            c=getattr(counts, key)#number of counts
+            binstring="{0:b}".format(int(key,0)).zfill(num_qubits)
+            y=[int(i) for i in binstring]
+            E[item] += cost_MaxCut(y,G)*c/shots
+    return E
