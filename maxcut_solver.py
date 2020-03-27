8d93341c1aebfb5829792018efe5defc8f0c3ce3
Giorgio Sartor <Giorgio.Sartor@sintef.no>
2020-03-27 20:56:51 +0100

    Add mip solver.

--- maxcut_solver.py
+++ maxcut_solver.py
@@ -3,8 +3,10 @@
 # All functions take as input a NetworkX graph and return the optimal solution.
 
 import networkx as nx
+import numpy as np
 import time
-import cylp
+from cylp.cy import CyCbcModel, CyClpSimplex
+from cylp.py.modeling.CyLPModel import CyLPModel, CyLPArray
 
 def enumerate(G):
     if (len(G) > 30):
@@ -26,10 +28,59 @@ def enumerate(G):
 
     return maxcut_value, maxcut
 
+def branch_and_bound(G):
+    N = len(G)
+    model = CyLPModel()
+    # Decision variables, one for each node
+    x = model.addVariable('x', N, isInt=True)
+    # Adjacency matrix (possibly weighted)
+    W = nx.to_numpy_matrix(G)
+    z_ind = dict()
+    ind = 0
+    w = []
+    for i in range(N):
+        j_range = range(N)
+        if (not nx.is_directed(G)):
+            # Reduced range for undirected graphs
+            j_range = range(i, N)
+        for j in j_range:
+            if (W[i,j] == 0):
+                continue
+            if (i not in z_ind):
+                z_ind[i] = dict()  
+            z_ind[i][j] = ind
+            w.append(W[i,j])
+            ind += 1
+    # Aux variables, one for each edge
+    z = model.addVariable('z', len(w), isInt=True)
+    # Adding the box contraints
+    model += 0 <= x <= 1
+    model += 0 <= z <= 1
+    # Adding the cutting constraints
+    # If x_i == x_j then z_ij = 0
+    # If x_i != x_j then z_ij = 1
+    for i in z_ind:
+        for j in z_ind[i]:
+            model += z[z_ind[i][j]] - x[i] - x[j] <= 0
+            model += z[z_ind[i][j]] + x[i] + x[j] <= 2
+    # Adding the objective function
+    model.objective = CyLPArray(w) * z
+    lp = CyClpSimplex(model)
+    lp.logLevel = 0
+    lp.optimizationDirection = 'max'
+    mip = lp.getCbcModel()
+    mip.logLevel = 0
+    mip.branchAndBound(-1)
+
+    return mip.objectiveValue, [int(i) for i in mip.primalVariableSolution['x']]
+
 if __name__ == "__main__":
     G = nx.erdos_renyi_graph(15, 0.6)
     for u,v in G.edges():
         G[u][v]['weight'] = 1
     t = time.time()
     print(enumerate(G))
+    print(time.time() - t)
+    t = time.time()
+    print(branch_and_bound(G))
     print(time.time() - t)
