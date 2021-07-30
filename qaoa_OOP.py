30587c53df251e0bbdf4f80f447817c6a7aef3c2
Sondre <sondre@duna.no>
2021-07-30 11:52:06 +0200

    Added objectoriented version of the qaoa, and the tailassignment problem

---
+++ qaoa_OOP.py
@@ -0,0 +1,279 @@
+import os 
+import sys
+
+sys.path.append('../')
+from qiskit_utilities.utilities import *
+from qiskit import execute
+from plots import * 
+
+import numpy as np
+import scipy.optimize as optimize
+
+import itertools
+
+class QAOABase:
+    
+    def __init__(self,options = None):
+        
+        self.options       = options
+        self.g_it          = 0
+        self.g_values      = dict()
+        self.g_best_values = dict()
+        self.g_params      = dict()
+        
+        self.params        = dict()
+        self.E             = dict()
+        self.best          = dict()
+
+    # -----------------------------------------
+    # Methods a child would have to implement:
+    
+    def initial_state(self):
+        raise NotImplementedError
+    
+    def createCircuit(self):
+        raise NotImplementedError
+
+    def measurementStatistics(self):
+        raise NotImplementedError
+
+    def cost(self):
+        raise NotImplementedError
+
+    def getval(self):
+        raise NotImplementedError
+
+    # -----------------------------------------
+
+    def interp_init(self):
+
+        x_prev = self.params[f'xL_d{self.depth - 1}']
+        print(f"P = {x_prev}")
+
+        # Minimize needs one-dimensional input, but the interpolation of the
+        # parameters can be done with a multidimensional input
+        #
+        #    gamma_1 gamma_2 gamma_3 ...
+        #    beta_1  beta_2  beta_3  ...
+        #    ...
+        # The resulting flattened vector will be
+        #    gamma_1 beta_1 gamma-2 beta_2 ...
+        # Which is what we want for the createCircuit method called in get-val
+
+        p      = self.depth - 1
+        
+        x_prev = x_prev.reshape((p,self.q))
+        x      = np.zeros((p+1,self.q))
+       
+        x[0,:] = x_prev[0,:]
+        
+        for i in range(2,p+1):
+            x[i - 1,:] = (i - 1)/p  * x_prev[i-2,:] + (p - i + 1)/p * x_prev[i-1,:]
+        
+        x[p,:] = x_prev[p-1,:]
+        print(f"P_ = {x.flatten()}")
+        self.params[f'x0_d{self.depth}'] = x.flatten()
+        
+        return x.flatten()
+
+    def save_current_params(self):
+
+        # Find the best value along the path
+        ind = max(self.g_values, key = self.g_values.get)
+
+        # Save params
+        
+        self.params[f'xL_d{self.depth}'] = self.g_params[ind].copy()
+        self.E[f'{self.depth}']          = self.g_values[ind]
+        self.best[f'{self.depth}']       = self.g_best_values[ind]
+
+    def get_energy_landscape(self):
+
+        print("Calculating energy landscape ...")
+
+        ranges = []
+        for i in range(self.q):
+
+            step = (self.params_ul[i] - self.params_ll[i])/self.params_n[i]
+            
+            ranges.append(slice(self.params_ll[i],self.params_ul[i],step))
+        
+        x0, fval, grid, Jout = optimize.brute(self.getval, ranges, full_output = True)
+
+        print("Calculating energy landscape done.")
+        # Flatten the parameters according to fortran ordering
+        self.params[f'x0_d{self.depth}'] = x0.flatten()
+        
+        # Transpose energy landscape to be in accordance with the ordering used in the
+        # runQAOA function used previously.
+        return Jout.T, x0.flatten()
+
+    def simulate_init(self, **simulation_args):
+        
+        # Unpack the simulation arguments:
+        
+        self.backend     = simulation_args['backend']
+        self.optmethod   = simulation_args['optmethod']
+        self.max_depth   = simulation_args['max_depth']
+        self.repeats     = simulation_args.get('repeats',1)
+        self.shots       = simulation_args.get('shots',  1)
+        self.noise_model = simulation_args.get('noise_model',None)
+
+        # Give lower and upper limits (ll, ul) as arrays rather than
+        # separately giving max and min for each variable, so that it is
+        # easier to change the number of variables later on
+        # Aslo, provide the number of steps for each variable as a similar array
+        
+        self.params_ll = simulation_args['params_ll']
+        self.params_ul = simulation_args['params_ul']
+        self.params_n  = simulation_args['params_n']
+
+        self.q         = self.params_ll.size
+
+        assert( self.params_ll.size == self.params_ul.size == self.params_n.size)
+        
+    def simulate(self, **simulation_args):
+
+        """
+        Simulation function for doing the QAOA algorithm.
+
+        Parameters
+        ----------
+        simulation_args : dict
+            Keyword arguments, must contain:
+                backend
+                optmethod
+                max_depth
+                params_ll : lower limits for each parameter to do brute search over
+                params_ul : upper limits -- " --
+                params_n  : number of points -- " --
+
+                
+        """
+
+        self.simulate_init(**simulation_args)
+        self.depth = 1
+        
+        # Calculate energy_landscape - global optimisation 
+
+        Elandscape, x0 = self.get_energy_landscape()
+
+        # Local optimisation
+
+        for rep in range(self.repeats):
+            print(f"Depth = {self.depth}, Rep = {rep + 1}")
+
+            res = optimize.minimize(self.getval,
+                                    x0 = x0,
+                                    method = self.optmethod,
+                                    options={'xatol': 1e-2, 'fatol': 1e-1, 'disp': True})
+
+        self.save_current_params()
+
+        while self.depth < self.max_depth:
+
+            self.depth += 1
+
+            x0 = self.interp_init()
+
+            # Reset the current book-keeping variables for each depth
+                
+            self.g_it          = 0
+            self.g_values      = dict()
+            self.g_best_values = dict()
+            self.g_params      = dict()
+
+            for rep in range(self.repeats):
+                print(f"Depth = {self.depth}, Rep = {rep + 1}")
+                res = optimize.minimize(self.getval,
+                                        x0 = x0,
+                                        method = self.optmethod,
+                                        options={'xatol': 1e-2, 'fatol': 1e-1, 'disp': True})
+
+            self.save_current_params()
+
+        return Elandscape, self.params, self.E, self.best
+            
+        
+            
+class QAOAStandard(QAOABase):
+
+    def __init__(self,qubits,options = None):
+        super().__init__(options)
+        self.generate_state_strings(qubits) 
+        
+    def generate_state_strings(self, qubits):
+        """
+        Generates an array of all the state strings in increasing order. 
+        Useful for toy examples.
+        
+        Parameters
+        ----------
+        qubits : int 
+            number of qubits in circuit.
+
+        """
+
+        self.state_strings = np.array([''.join(i) for i in itertools.product('01', repeat= qubits)])        
+        # Flip the state-vectors to be in accordance with the ordering in qiskit:
+        #self.state_strings = map(lambda x : x[::-1], self.state_strings)
+
+    def initial_state(self, qubits):
+        # Save current circuit and registers as
+        # member variables of the object
+        # Use |+>^n as initial state
+
+        q = QuantumRegister(qubits)
+        c = ClassicalRegister(qubits)
+        qc = QuantumCircuit(q,c, name = self.options.get('name', None))
+
+        qc.h(range(qubits))
+
+        self.q_register = q
+        self.c_register = c
+        self.qc         = qc
+
+    def getval(self, params):
+        """
+        Objective function to use in the minimizer.
+
+        Parameters
+        ----------
+        params : array
+            Array of parameters to feed into the circuit: gamma_1, beta_1, ... , beta_p
+            or even more parameters per. layer.
+        
+        Returns
+        -------
+        -val[0] : float
+            Value of hamiltonian for these parameters. 
+
+        """
+        
+
+        self.g_it += 1
+
+        circuit = self.createCircuit(params)
+
+        if self.backend.configuration().local:
+            job = execute(circuit,
+                          backend = self.backend,
+                          noise_model = self.noise_model,
+                          shots = self.shots)
+        else:
+            job = start_or_retrieve_job(name +"_"+str(g_it),
+                                        self.backend,
+                                        circuit,
+                                        options = {'shots' : self.shots})
+
+        val, _, bval = self.measurementStatistics(job)
+
+        self.g_values[str(self.g_it)]      = val[0]
+        self.g_best_values[str(self.g_it)] = bval
+        self.g_params[str(self.g_it)]      = params
+
+        return -val[0]
+
+
+
+
---
+++ tailassignment_oop.py
@@ -0,0 +1,376 @@
+from qaoa_OOP import *
+
+class QAOATailAssignment(QAOAStandard):
+
+    def __init__(self,options):
+        super().__init__(options['CR'].size, options)
+
+        # Vectorized functions for the cost and is_solution:
+        # Very useful for simulation with the statevector, as it
+        # allows for avoiding multiple nested loops in the measurementStatistics function
+        
+        self.vector_cost = np.vectorize(self.cost)
+        self.vector_is_solution = np.vectorize(self.is_solution)
+
+        # Save matrices as variables of object:
+        
+        self.FR         = self.options.get('FR', None)
+        self.CR         = self.options.get('CR', None)
+        self.mu         = self.options.get('mu', 1)
+
+        self.F, self.R  = np.shape(self.FR)
+
+
+    def cost(self,binstring):
+        
+        # Reverse string since qiskit uses ordering MSB ... LSB
+        x = np.array(list(map(int,binstring[::-1])))
+        return - ( (self.CR @ x) + self.mu * np.sum((1 - (self.FR @ x))**2) )
+
+    def mix_states(self,beta):
+        """
+        Applies unitary evolution of mixer hamiltonian with 
+        time parameter beta
+
+        Parameters
+        ----------
+        beta : float
+            Time/angle for applying hamiltonian.
+
+        """
+        self.qc.rx( - 2 * beta, self.q_register ) 
+
+    def apply_exco(self,gamma):
+        """
+        Applies unitary evolution of exact cover hamiltonian with 
+        time-parameter gamma
+
+        Parameters
+        ----------
+        gamma : float
+            Time/angle for applying hamiltonian.
+
+        """
+        
+        for r in range(self.R):
+            hr = 0.5 * self.mu * self.FR[:,r] @ (np.sum(self.FR,axis = 1) - 2)
+
+            self.qc.rz( gamma * hr, self.q_register[r])
+
+            for r_ in range(r+1,self.R):
+                Jrr_  = 0.5 * self.FR[:,r] @ self.FR[:,r_]
+
+                # Apply U(gamma), coupling part
+
+                self.qc.cx(self.q_register[r], self.q_register[r_])
+                self.qc.rz(gamma * Jrr_, self.q_register[r_])
+                self.qc.cx(self.q_register[r], self.q_register[r_])
+
+    def apply_cost(self,gamma):
+        """
+        Applies unitary evolution of cost hamiltonian with 
+        time-parameter gamma
+
+        Parameters
+        ----------
+        gamma : float
+            Time/angle for applying hamiltonian.
+
+        """
+
+        for r in range(self.R):
+            hr = 0.5 * self.CR[r]
+
+            self.qc.rz(gamma * hr, self.q_register[r])
+
+    def apply_hamiltonian(self,gamma):
+        """
+        Applies unitary evolution of the full hamiltonian representing the 
+        problem, with time parameter gamma
+
+        Parameters
+        ----------
+        gamma : float
+            Time/angle for applying hamiltonian.
+
+        """
+        
+        for r in range(self.R):
+            hr = 0.5 * self.CR[r] + 0.5 * self.mu * self.FR[:,r] @ (np.sum(self.FR,axis = 1) - 2)
+
+            self.qc.rz( gamma * hr, self.q_register[r])
+
+            for r_ in range(r+1,self.R):
+                Jrr_  = 0.5 * self.FR[:,r] @ self.FR[:,r_]
+
+                # Apply U(gamma), coupling part
+
+                self.qc.cx(self.q_register[r], self.q_register[r_])
+                self.qc.rz(gamma * Jrr_, self.q_register[r_])
+                self.qc.cx(self.q_register[r], self.q_register[r_])
+            
+    def createCircuit(self, params):
+
+        """
+        Implements the ciruit for the tail assignment problem
+        Parameters
+        ----------
+        params : array
+            variational parameters gamma_1 beta_1, ... , gamma_p beta_p
+
+        Returns
+        -------
+        qc : QuantumCircuit    
+
+        """
+
+        self.initial_state(self.R)
+        
+        gammas = params[::2]
+        betas  = params[1::2]
+
+        for d in range(self.depth):
+
+            gamma = gammas[d]
+            beta  = betas[d]
+
+            # Hamiltonian - cost + constraint
+            self.apply_hamiltonian(gamma)
+            # This is an equivalent implementation, but requires more gates.
+            # as the h-terms are not collected together
+            #self.apply_cost(gamma)
+            #self.apply_exco(gamma)
+
+            if self.options['usebarrier']:
+                self.qc.barrier()
+
+            # Apply mixer U(beta):
+            self.mix_states(beta)
+            
+            if self.options['usebarrier']:
+                self.qc.barrier()
+                
+        if "statevector" not in self.backend.name().split('_'):
+            # Do not measure at the end of the circuit if using a
+            # statevector simulation 
+            self.qc.measure(self.q_register,self.c_register)
+
+        return self.qc
+
+    def simulation_statistics(self, plot = True, savefig = None):
+        """
+        Do simulation again with optimal found parameters and 
+        return the success probability together with the average hamiltonian.
+
+        Parameters
+        ----------
+        plot : bool
+            If True, plot the results
+        savefig : string or None
+            If not None, save the figure with the filename/path given by savefig 
+
+        Returns
+        -------
+        SP : array
+            Success probability as a function of depth
+        C : array
+            Total cost as a function of depth.
+        """
+
+        SP = np.zeros(self.max_depth)
+        C  = np.zeros(self.max_depth)
+        
+        self.depth = 1
+        while self.depth <= self.max_depth:
+
+            qc  = self.createCircuit(self.params[f'xL_d{self.depth}'])
+            job = execute(qc,
+                          backend = self.backend,
+                          noise_model = self.noise_model,
+                          shots = self.shots)
+            
+            SP[self.depth - 1] = self.successProbability(job)
+
+            statevector = job.result().get_statevector()
+            #probs       = statevector.conjugate() # temporarily save the conjugated statevector here
+            #np.multiply(statevector, probs, out = probs)
+
+            probs = (np.abs(statevector))**2
+            
+            C[self.depth - 1 ] = self.vector_cost(self.state_strings) @ probs
+
+            self.depth += 1
+        if plot:
+            plot_H_prob(self,SP,C, savefig)
+
+        return SP, C
+
+    def is_solution(self,binstring):
+        a = np.array(list(map(int,binstring[::-1])))
+        return np.all(np.sum(self.FR * a,1)-1 == 0)
+
+    def successProbability(self,job):
+        """
+        Calculation of the success probability for a given job
+        Separating the calculation for a simulation done with the 
+        statevector is a bit hackey.
+
+        Parameters
+        ----------
+        job : Qiskit job
+           
+        Returns
+        -------
+        s_prob : float
+            Success probability 
+        """
+        
+        
+        if "statevector" in self.backend.name().split('_'):
+            
+            experiment_results = job.result().results
+            statevector = job.result().get_statevector()
+            probs = (np.abs(statevector))**2
+
+            s_prob = self.vector_is_solution(self.state_strings) @ probs
+            
+        else:
+            
+            experiment_results = job.result().results
+            s_prob = 0
+            # assumes experiment results is a one-dimensional array
+
+            for result in experiment_results:
+                n_shots = result.shots
+                counts = result.data.counts
+                for hexkey in list(counts.keys()):
+                    count = counts[hexkey]
+                    binstring = "{0:b}".format(int(hexkey,0)).zfill(rN)
+                    if is_Solution(binstring, FR):
+                        s_prob += count/n_shots
+            
+        return s_prob
+
+    def measurementStatistics(self,job):
+        """
+        Measurement statistics for the tail assignment problem.
+        Calculating only the expectations and the best cost
+
+        Parameters
+        ----------
+        job : Qiskit job
+            ...
+
+        Returns
+        -------
+            expectation_values : array
+            variances          : array
+            cost_best          : float 
+
+        """
+
+        R         = np.size(self.FR[0,:])
+        cost_best = - np.inf
+        experiment_results = job.result().results
+        expectations = np.zeros(len(experiment_results))
+
+        ## Simulation done with statevector
+        ## Much faster than the latter, as many operations are
+        ## vectorized here and don't depend on iterating through dictionaries
+        
+        if "statevector" in self.backend.name().split('_'):
+
+            statevector = job.result().get_statevector()
+            #probs       = statevector.conjugate() # temporarily save the conjugated statevector here
+            #np.multiply(statevector, probs, out = probs)
+
+            probs = np.abs(statevector)**2
+
+            costs = self.vector_cost(self.state_strings)
+            E     = costs @ probs
+
+            best_sampled_state = np.max( costs @ np.ceil(probs))
+            cost_best          = max(cost_best,best_sampled_state)
+
+            expectations[0] = E 
+
+        ## Simulation done with noisy simulation
+        
+        else:
+            
+           for i, result in enumerate(experiment_results):
+               n_shots = result.shots
+               counts = result.data.counts
+
+               E  = 0
+
+               for hexkey in list(counts.keys()):
+                   count     = counts[hexkey]
+                   binstring = "{0:b}".format(int(hexkey,0)).zfill(R)
+                   cost      = self.cost(binstring)
+                   cost_best = max(cost_best, cost)
+                   E        += cost*count/n_shots
+
+               expectations[i] = E
+               
+
+        return expectations, None , cost_best
+
+
+class TailAssignmentInterlaced(QAOATailAssignment):
+
+    def createCircuit(self, params):
+
+        """
+        Implements the ciruit for the tail assignment problem, with three parameters
+        Parameters
+        ----------
+        params : array
+            variational parameters gamma_1 beta_1 delta_1 , ... , gamma_p beta_p delta_p
+
+        Returns
+        -------
+        qc : QuantumCircuit    
+
+        """
+
+        self.initial_state(self.R)
+        
+        gammas = params[::3]
+        betas  = params[1::3]
+        deltas = params[2::3]
+
+        for d in range(self.depth):
+
+            gamma = gammas[d]
+            beta  = betas[d]
+            delta = deltas[d]
+
+            # Hamiltonian - weights 
+            self.apply_cost(delta)
+
+            if self.options['usebarrier']:
+                self.qc.barrier()
+
+             # Apply mixer U(beta) inbetween hamiltonians
+            self.mix_states(beta)
+
+            if self.options['usebarrier']:
+                self.qc.barrier()
+
+            self.apply_exco(gamma)
+
+            # Apply mixer U(beta) at the end
+            self.mix_states(beta)
+            
+            if self.options['usebarrier']:
+                self.qc.barrier()
+                
+        if "statevector" not in self.backend.name().split('_'):
+            # Do not measure at the end of the circuit if using a
+            # statevector simulation 
+            self.qc.measure(self.q_register,self.c_register)
+
+        return self.qc
+
+    
